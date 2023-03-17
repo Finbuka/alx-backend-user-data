7ffb107136e9b6893799b4ef80518d83580379b4
Finbuka <ebukajoe100@gmail.com>
2023-03-17 10:01:37 +0100

    codestyle

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -67,6 +67,7 @@ def profile() -> str:
         abort(403)
     return jsonify({"email": "{}".format(user.email)}), 200
 
+
 @app.route("/reset_password", methods=["POST"], strict_slashes=False)
 def get_reset_password_token() -> str:
     """POST /reset_password"""
